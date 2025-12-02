5beab0ec20a9370554ff36bc08306904c9d8296e
Касич Полина Олеговна <alice.sova@mail.ru>
2025-12-02 16:51:17 +0500

    add github workflow

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,14 @@ module.exports = {
     testEnvironment: 'node',
     testMatch: ['**/*.test.ts'],
     collectCoverage: true,
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 85,
+            lines: 85,
+            statements: 85
+        }
+    },
     coverageDirectory: 'coverage',
     moduleFileExtensions: ['ts', 'js', 'json'],
     globals: {
