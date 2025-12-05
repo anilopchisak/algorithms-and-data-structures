f0db8b9057b750c310334414d13b1ecbeedcd84b
Касич Полина Олеговна <alice.sova@mail.ru>
2025-12-05 10:50:02 +0500

    add insertion sort

--- tasks/sortings/sorts/insertion.ts
+++ tasks/sortings/sorts/insertion.ts
@@ -1,5 +1,13 @@
 const insertionSort = (arr: number[]): number[] => {
-
+    for (let i = 0; i < arr.length - 1; i++) {
+        let j = i + 1
+        while (j > 0) {
+            if (arr[j] < arr[j - 1]) {
+                [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]]
+            }
+            j--
+        }
+    }
     return arr
 }
 
